ef325e7c089ce0e4d571c0fff06951c3b41627dd
slince <taosikai@yeah.net>
2021-12-26 18:10:02 +0800

    change context to runtime

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,9 +1,16 @@
 module.exports = {
-    entry:  __dirname + "/src/expression.js",
+    entry:  __dirname + "/src/expression.ts",
+    mode: "development",
     output: {
         path:  __dirname + "/dist",
         filename: "expression.js",
         libraryTarget: "umd",
         library: "expression"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+       rules: [{test: /\.tsx?$/, use: "ts-loader"}]
     }
 }
